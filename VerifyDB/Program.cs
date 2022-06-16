0ad870d5d04e85daa6fcc62cc8214cc096a853d4
Timaty <101876583+T1maty@users.noreply.github.com>
2022-06-16 14:19:06 +0300

    Initial commit

---
+++ VerifyDB/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using VerifyDB.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
